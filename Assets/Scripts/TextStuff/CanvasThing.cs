2226ef1d5aa620879dbd3fa57559c27e0f63bfda
michig23 <michael.higgins@students.mccc.edu>
2025-03-03 17:04:14 -0500

    Pause Menu Update

--- Assets/Scripts/TextStuff/CanvasThing.cs
+++ Assets/Scripts/TextStuff/CanvasThing.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class CanvasThing : MonoBehaviour
 {
-    public  int berryCount;
+    public int berryCount;
     public int fishCount;
     public int honeyCount;
 
@@ -19,25 +19,44 @@ public class CanvasThing : MonoBehaviour
     TMP_Text textBoxText;
     Image textBoxPortrait;
 
+    Image pauseMenu;
+
+    Button backButton;
+    Button mapButton;
+    Button exitButton;
+
+    float changeSpeed = 0.05f;
+
     public Sprite bearPortrait;
     bool keyPressed = false;
 
     private void Awake()
     {
-       berryText = transform.GetChild(0).GetComponent<TMP_Text>();
-       fishText = transform.GetChild(1).GetComponent<TMP_Text>();
-       honeyText = transform.GetChild(2).GetComponent<TMP_Text>();
-       textBox = transform.GetChild(3).GetComponent<Image>();
-       textBoxText = textBox.transform.GetChild(0).GetComponent<TMP_Text>();
-       textBoxPortrait = textBox.transform.GetChild(1).GetComponent<Image>();
-       DontDestroyOnLoad(gameObject);
+        berryText = transform.GetChild(1).GetComponent<TMP_Text>();
+        fishText = transform.GetChild(2).GetComponent<TMP_Text>();
+        honeyText = transform.GetChild(3).GetComponent<TMP_Text>();
+        textBox = transform.GetChild(4).GetComponent<Image>();
+        textBoxText = textBox.transform.GetChild(0).GetComponent<TMP_Text>();
+        textBoxPortrait = textBox.transform.GetChild(1).GetComponent<Image>();
+
+        pauseMenu = transform.GetChild(0).GetComponent<Image>();
+        backButton = pauseMenu.transform.GetChild(1).GetComponent<Button>();
+        mapButton = pauseMenu.transform.GetChild(2).GetComponent<Button>();
+        exitButton = pauseMenu.transform.GetChild(3).GetComponent<Button>();
+
+        backButton.onClick.AddListener(BackButton);
+        mapButton.onClick.AddListener(MapButton);
+        exitButton.onClick.AddListener(ExitButton);
+
+        DontDestroyOnLoad(gameObject);
     }
     void Start()
     {
-        
+
     }
     void Update()
     {
+        ReduceBFH();
         if (Input.anyKeyDown == true)
         {
             keyPressed = true;
@@ -46,6 +65,66 @@ public class CanvasThing : MonoBehaviour
         {
             keyPressed = false;
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenu.transform.gameObject.activeSelf == false)
+            {
+                Time.timeScale = 0;
+                pauseMenu.transform.gameObject.SetActive(true);
+            }
+            else if (pauseMenu.transform.gameObject.activeSelf == true)
+            {
+                Time.timeScale = 1;
+                pauseMenu.transform.gameObject.SetActive(false);
+            }
+        }
+    }
+
+
+
+    void ReduceBFH()
+    {
+
+
+        if (FindAnyObjectByType<Bear>().GetComponent<Rigidbody2D>().linearVelocityX != 0 && pauseMenu.transform.gameObject.activeSelf == false)
+        {
+            berryText.color = Color.Lerp(berryText.color, new Color(berryText.color.r, berryText.color.g, berryText.color.b, 0.25f), changeSpeed);
+            berryText.transform.GetChild(0).GetComponent<Image>().color = Color.Lerp(berryText.transform.GetChild(0).GetComponent<Image>().color, new Color(255, 255, 255, 0.25f), changeSpeed);
+
+            fishText.color = Color.Lerp(fishText.color, new Color(fishText.color.r, fishText.color.g, fishText.color.b, 0.25f), changeSpeed);
+            fishText.transform.GetChild(0).GetComponent<Image>().color = Color.Lerp(fishText.transform.GetChild(0).GetComponent<Image>().color, new Color(255, 255, 255, 0.25f), changeSpeed);
+
+            honeyText.color = Color.Lerp(honeyText.color, new Color(honeyText.color.r, honeyText.color.g, honeyText.color.b, 0.25f), changeSpeed);
+            honeyText.transform.GetChild(0).GetComponent<Image>().color = Color.Lerp(honeyText.transform.GetChild(0).GetComponent<Image>().color, new Color(255, 255, 255, 0.25f), changeSpeed);
+        }
+        else
+        {
+
+            berryText.color = Color.Lerp(berryText.color, new Color(berryText.color.r, berryText.color.g, berryText.color.b, 1), changeSpeed);
+            berryText.transform.GetChild(0).GetComponent<Image>().color = Color.Lerp(berryText.transform.GetChild(0).GetComponent<Image>().color, new Color(255, 255, 255, 1), changeSpeed);
+
+            fishText.color = Color.Lerp(fishText.color, new Color(fishText.color.r, fishText.color.g, fishText.color.b, 1), changeSpeed);
+            fishText.transform.GetChild(0).GetComponent<Image>().color = Color.Lerp(fishText.transform.GetChild(0).GetComponent<Image>().color, new Color(255, 255, 255, 1), changeSpeed);
+
+            honeyText.color = Color.Lerp(honeyText.color, new Color(honeyText.color.r, honeyText.color.g, honeyText.color.b, 1), changeSpeed);
+            honeyText.transform.GetChild(0).GetComponent<Image>().color = Color.Lerp(honeyText.transform.GetChild(0).GetComponent<Image>().color, new Color(255, 255, 255, 1), changeSpeed);
+        }
+    }
+    void BackButton()
+    {
+        pauseMenu.transform.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    void MapButton()
+    {
+        //put map stuff here
+    }
+
+    void ExitButton()
+    {
+        //put exit stuff here
     }
 
     public void AddBerry()
