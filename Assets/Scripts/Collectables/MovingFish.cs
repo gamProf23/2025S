418e880a53dfc52e0c0d0c69eef1ed8fb002dfa0
michig23 <michael.higgins@students.mccc.edu>
2025-03-10 17:33:18 -0400

    Added Salmon and Moving Fish

---
+++ Assets/Scripts/Collectables/MovingFish.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class MovingFish : MonoBehaviour
+{
+    public int fishSpeed;
+    bool movingRight = false;
+    float scaleX;
+    float scaleXNeg;
+
+    private void Awake()
+    {
+        scaleX = transform.localScale.x;
+        scaleXNeg = transform.localScale.x * -1;
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeDirection();
+
+        if (movingRight == false)
+        {
+            transform.Translate(Vector2.left * fishSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.right * fishSpeed * Time.deltaTime);
+        }
+    }
+
+    void ChangeDirection()
+    {
+        Vector2 startPos;
+        Vector2 endPos;
+        RaycastHit2D hit;
+
+        if (movingRight == true)
+        {
+            startPos = gameObject.transform.position;
+            endPos = gameObject.transform.right * 2f;
+        }
+        else
+        {
+            startPos = gameObject.transform.position;
+            endPos = gameObject.transform.right * -2f;
+        }
+
+        Debug.DrawRay(startPos, endPos);
+
+        hit = Physics2D.Raycast(startPos, endPos, 2f);
+
+        if (hit.transform != null && (hit.transform.tag == "Ground" || hit.transform.tag == "Slope"))
+        {
+            if (movingRight == false)
+            {
+                movingRight = true;
+                transform.localScale = new Vector3(scaleXNeg, transform.localScale.y, 1);
+            }
+            else
+            {
+                movingRight = false;
+                transform.localScale = new Vector3(scaleX, transform.localScale.y, 1);
+            }
+        }
+    }
+}
