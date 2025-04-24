41f716984327beb1ef452b5e5ae2ac1532b79e81
michig23 <michael.higgins@students.mccc.edu>
2025-04-24 13:28:08 -0400

    Update SceneInfo.cs

--- Assets/Scripts/SceneInfo.cs
+++ Assets/Scripts/SceneInfo.cs
@@ -127,6 +127,8 @@ public class SceneInfo : MonoBehaviour
 
     public static ExitDirections exitDirection;
 
+
+
     private void Awake()
     {
         FindExits();
@@ -722,6 +724,10 @@ public class SceneInfo : MonoBehaviour
 
     }
 
+    int GetCMAmount()
+    {
+        return 0;
+    }
     
 
     public static partial class SceneHelper
