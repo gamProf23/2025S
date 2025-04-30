00ff1011d89e732f59cb41b9bc7a53679ffe8881
JalenLloyd <jalen.lloyd20@gmail.com>
2025-04-30 16:52:56 -0400

    Wind Toggle Added

--- Assets/Scripts/Wind.cs
+++ Assets/Scripts/Wind.cs
@@ -5,8 +5,8 @@ using UnityEngine.U2D;
 public class Wind : MonoBehaviour
 {
     public float windTimer;
-    public bool windOn = true;
-
+    //public bool windOn = true;
+    public bool tempWind = true;
     private void Awake()
     {
        
@@ -21,23 +21,26 @@ public class Wind : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        windTimer = windTimer + Time.deltaTime;
-        if (windTimer >= 5f)
+        if (tempWind)
         {
-            
-            windOn = false;
-            GetComponentInChildren<SpriteShapeRenderer>().enabled = false;
-            GetComponent<PolygonCollider2D>().enabled = false;
-            GetComponent<AreaEffector2D>().enabled = false;
-            
-        }
-        if (windTimer >= 8f)
-        {
-            windOn = true;
-            GetComponentInChildren<SpriteShapeRenderer>().enabled = true;
-            GetComponent<PolygonCollider2D>().enabled = true;
-            GetComponent<AreaEffector2D>().enabled = true;
-            windTimer = 0f;
-        }
+            windTimer = windTimer + Time.deltaTime;
+            if (windTimer >= 5f)
+            {
+
+                //windOn = false;
+                GetComponentInChildren<SpriteShapeRenderer>().enabled = false;
+                GetComponent<PolygonCollider2D>().enabled = false;
+                GetComponent<AreaEffector2D>().enabled = false;
+
+            }
+            if (windTimer >= 8f)
+            {
+                //windOn = true;
+                GetComponentInChildren<SpriteShapeRenderer>().enabled = true;
+                GetComponent<PolygonCollider2D>().enabled = true;
+                GetComponent<AreaEffector2D>().enabled = true;
+                windTimer = 0f;
+            }
+        }        
     }
 }
